195731ea1c90d563689f40e73b54f23a7882be8d
0lga <otoiker@yandex.ru>
2022-12-30 13:23:45 +0300

    Itog_sem9

---
+++ 9SeminarItogDZ/DZ64Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке
+// от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите натуральное число N больше 1: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number <= 0) Console.Write($"{number} - не натуральное число ");
+Console.Write($"N = {number} -> ");
+Numbers(number);
+
+void Numbers(int n)
+{
+    if (n <= 0)
+    return;
+    if (n == 1) Console.Write($"{n}");
+    if (n > 1) Console.Write($"{n}, ");
+    Numbers(n - 1);
+}
---
+++ 9SeminarItogDZ/DZ66Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// /Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке
+// от M до N.
+// M = 1; N = 15-> 120
+// M = 4; N = 8. -> 30
+Console.Write("Введите натуральное число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите натуральное число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write($"M = {m}; N = {n} -> ");
+SummMN(m, n);
+void SummMN(int m, int n)
+{
+    Console.Write(Sum(m - 1, n));
+}
+int Sum(int m, int n)
+{
+    int result = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        result = m + Sum(m, n);
+        return result;
+    }
+}
